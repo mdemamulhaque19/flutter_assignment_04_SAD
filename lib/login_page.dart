3b5874e7724b45ca525ff4fcda92d2999cd27769
mdemamulhaque19 <cse_0182410012101098@lus.ac.bd>
2026-05-20 19:02:00 +0600

    Login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,9 @@ import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment_4/signup_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,125 +15,137 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
 
-  final TextEditingController textEditingController = TextEditingController();
-  final TextEditingController passwordEditingController = TextEditingController();
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _isProgress = false;
+  final _supabase = Supabase.instance.client;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(80.0),
-        child: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 45,),
-              Text('Login',
-                style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.purple,
-                    fontWeight: FontWeight.bold
+      // appBar: AppBar(
+      //   backgroundColor: Colors.purple,
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 120,),
+                Text('Login',
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.purple,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
-              ),
 
-              SizedBox(height: 10,),
+                SizedBox(height: 10,),
 
-              TextFormField(
-                controller: textEditingController,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    hintStyle: TextStyle(
-                        color: Colors.white
-                    ),
-                    hintText: 'Email',
-                  prefixIconColor: Colors.black,
-                ),
-                validator: (String? value) {
-                  String inputText = value ?? '';
-                  if(EmailValidator.validate(inputText)== false){
-                    return 'Enter a valid email';
-                  }
-                  return null;
-                },
-              ),
-
-              SizedBox(height: 10,),
-
-              TextFormField(
-                controller: passwordEditingController,
-                textInputAction: TextInputAction.done,
-                obscureText: true,
-
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    hintStyle: TextStyle(
-                        color: Colors.white
-                    ),
-                    hintText: 'Password',
-                  prefixIconColor: Colors.black,
-                ),
+                TextFormField(
+                  controller: _emailTEController,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      hintStyle: TextStyle(
+                          color: Colors.white
+                      ),
+                      hintText: 'Email',
+                    prefixIconColor: Colors.black,
+                  ),
                   validator: (String? value) {
-                    if((value?.length ?? 0) <= 6){
-                      return 'Password should more than 6 letters';
+                    String inputText = value ?? '';
+                    if(EmailValidator.validate(inputText)== false){
+                      return 'Enter a valid email';
+                    }
+                    if(RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value!) == false){
+                      return 'Enter a valid email';
                     }
                     return null;
-                  }
-              ),
-              SizedBox(height: 12,),
-              FilledButton(onPressed: (){
-                _onTapFilledButton();
-              },
-                  child: Icon(Icons.arrow_circle_right_outlined)),
-              SizedBox(height: 12,),
-              Center(
-                  child: Column(
-                    children: [
-                      TextButton(onPressed: (){},
-                          child: Text('Forget Password?')
+                  },
+                ),
+
+                SizedBox(height: 10,),
+
+                TextFormField(
+                  controller: _passwordTEController,
+                  textInputAction: TextInputAction.done,
+                  obscureText: true,
+
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      hintStyle: TextStyle(
+                          color: Colors.white
                       ),
-                      Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Center(
-                              child: RichText(
-                                text: TextSpan(
-                                    text: 'Don\'t have an account?',
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w600
-                                    ),
-                                    children: [
-
-                                      TextSpan(
-                                          text: ' Sign-Up',
-                                          style: TextStyle(
-                                              color: Colors.purple
-                                          ),
-                                          recognizer: TapGestureRecognizer()..onTap = (){
-                                            _onTapSignUpButton();
-                                          }
-                                      )
-                                    ]
-                                ),
-
-                              ),
-                            )
-                          ],
+                      hintText: 'Password',
+                    prefixIconColor: Colors.black,
+                  ),
+                    validator: (String? value) {
+                      if((value?.length ?? 0) <= 6){
+                        return 'Password should more than 6 letters';
+                      }
+                      if(!RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$').hasMatch(value!)){
+                        return 'Enter a valid password';
+                      }
+                      return null;
+                    }
+                ),
+                SizedBox(height: 12,),
+                FilledButton(onPressed: (){
+                  _onTapFilledButton();
+                },
+                    child: _isProgress ? CircularProgressIndicator() : Text('Login'),
+                ),
+                SizedBox(height: 12,),
+                Center(
+                    child: Column(
+                      children: [
+                        TextButton(onPressed: (){},
+                            child: Text('Forget Password?')
                         ),
-                      )
-                    ],
-                  )
-              ),
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Center(
+                                child: RichText(
+                                  text: TextSpan(
+                                      text: 'Don\'t have an account?',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600
+                                      ),
+                                      children: [
 
-            ],
+                                        TextSpan(
+                                            text: ' Sign-Up',
+                                            style: TextStyle(
+                                                color: Colors.purple
+                                            ),
+                                            recognizer: TapGestureRecognizer()..onTap = (){
+                                              _onTapSignUpButton();
+                                            }
+                                        )
+                                      ]
+                                  ),
+
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
@@ -144,17 +159,55 @@ class _LoginPageState extends State<LoginPage> {
 
   void _onTapFilledButton(){
     if(_formKey.currentState!.validate()){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Login is done')));
+      login();
     }
     else{
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Something went wrong')));
     }
   }
 
+  void login() async {
+    String email = _emailTEController.text.trim();
+    String password = _passwordTEController.text.trim();
+
+    setState(() {
+      _isProgress = true;
+    });
+
+    try {
+
+      await _supabase.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ),
+      );
+
+      _emailTEController.clear();
+      _passwordTEController.clear();
+
+    } on AuthApiException catch (e) {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(e.message)),
+      );
+
+    }
+
+    setState(() {
+      _isProgress = false;
+    });
+  }
+
   @override
   void dispose(){
     super.dispose();
-    textEditingController.dispose();
-    passwordEditingController.dispose();
+    _emailTEController.dispose();
+    _passwordTEController.dispose();
   }
 }
